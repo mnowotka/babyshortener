d938d403ad7478c6241d9e3d2bf328a40ef7ea87
mnowotka <mmmnow@gmail.com>
2017-10-08 22:48:00 +0100

    version bump to activate travis

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='babyshortener',
-    version='0.0.1',
+    version='0.0.2',
     entry_points={
         'console_scripts': [
             'run_shortener=babyshortener.run_shortener:main']
